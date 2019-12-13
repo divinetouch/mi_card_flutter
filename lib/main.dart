5b5ed8276c793edb961576051849dcc5534f6f50
Divine Touch <divine.touch07@gmail.com>
2019-12-13 16:39:07 -0600

    font, image, card

--- lib/main.dart
+++ lib/main.dart
@@ -11,28 +11,74 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Container(
-                height: 100,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/divine.jpeg'),
+              ),
+              Text(
+                'Divine Touch',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 35,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'SOFTWARE DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal[100],
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Card(
                 color: Colors.white,
-                child: Text('Container 1'),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+1 123 456 8901',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                height: 100,
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                height: 100,
-                color: Colors.red,
-                child: Text('Container 4'),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'divine.touch07@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
